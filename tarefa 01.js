bd251960c1fae6fa75a53103efc839eb8127148c
Esther Moraes Nascimento <91493517+esthermoraes@users.noreply.github.com>
2021-12-12 15:42:15 -0300

    Update tarefa 01.js

--- tarefa 01.js
+++ tarefa 01.js
@@ -2,20 +2,21 @@ const textos = document.querySelectorAll('h2');
 const linhas = new Array();
 textos.forEach(texto => { 
 	linhas.push(texto.textContent);
-	const ancora = document.createElement('a');
-	ancora.setAttribute('name', texto.textContent);
-	texto.append(ancora);
-	const retorno = document.createElement('a');
-	retorno.setAttribute('href', '#');
-	retorno.textContent = "Voltar ao sumário";
-	texto.parentElement.insertBefore(retorno, texto.nextElementSibling);
+	const apoio = document.createElement('a');
+	apoio.setAttribute('name', texto.textContent);
+	texto.append(apoio);
+	const voltar = document.createElement('a');
+	voltar.setAttribute('href', '#');
+	voltar.textContent = "Voltar ao sumário";
+	texto.parentElement.insertBefore(voltar, texto.nextElementSibling);
 });
+
 const lista = document.querySelector('ol');
 linhas.forEach(linha => { 
-	const item = document.createElement('li');
-	lista.append(item);
+	const unid = document.createElement('li');
+	lista.append(unid);
 	const link = document.createElement('a');
 	link.setAttribute('href', `#${linha}`);
 	link.textContent = linha;
-	item.append(link);
+	unid.append(link);
 });
